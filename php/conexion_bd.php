07771e326390a8d01603bde25f7aed52a8a9e4fa
Ilder Gutierrez <125604310+ildergutierrez@users.noreply.github.com>
2024-05-12 13:24:12 -0500

    a

--- php/conexion_bd.php
+++ php/conexion_bd.php
@@ -2,11 +2,11 @@
 // Credenciales de la base de datos
 session_start();
 
-$BD_HOST = $_ENV["BD_HOST"];
-$BD_USER = $_ENV["BD_USER"];
-$BD_PASSWORD = $_ENV["BD_PASSWORD"];
-$BD_NAME = $_ENV["BD_NAME"];
-$BD_PORT = $_ENV["BD_PORT"];
+$BD_HOST = $_ENV['BD_HOST'];
+$BD_USER = $_ENV['BD_USER'];
+$BD_PASSWORD = $_ENV['BD_PASSWORD'];
+$BD_NAME = $_ENV['BD_NAME'];
+$BD_PORT = $_ENV['BD_PORT'];
 
 $BD = mysqli_connect("$BD_HOST", "$BD_USER", "$BD_PASSWORD", "$BD_NAME", "$BD_PORT");
 // $host = getenv('roundhouse.proxy.rlwy.net');
