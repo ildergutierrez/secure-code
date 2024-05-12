0bf53a2e1a0e8978e357216f6a219a753033ed1e
Ilder Gutierrez <125604310+ildergutierrez@users.noreply.github.com>
2024-05-12 14:08:17 -0500

    ?????

--- php/conexion_bd.php
+++ php/conexion_bd.php
@@ -2,11 +2,11 @@
 // Credenciales de la base de datos
 session_start();
 echo"---------------------------------------------------------------------";
-$BD_HOST = $_ENV["monorail.proxy.rlwy.net"];
-$BD_USER = $_ENV["root"];
-$BD_PASSWORD = $_ENV["NMASJkGOmTCTOUIFXxyrKnKpnqYrwgHl"];
-$BD_NAME = $_ENV["railway"];
-$BD_PORT = $_ENV["55101"];
+$BD_HOST = $_ENV['monorail.proxy.rlwy.net'];
+$BD_USER = $_ENV['root'];
+$BD_PASSWORD = $_ENV['NMASJkGOmTCTOUIFXxyrKnKpnqYrwgHl'];
+$BD_NAME = $_ENV['railway'];
+$BD_PORT = $_ENV['55101'];
 
 
 $BD = mysqli_connect("$BD_HOST","$BD_USER", "$BD_PASSWORD","$BD_NAME","$BD_PORT");
