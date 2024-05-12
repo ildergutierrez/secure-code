f641e8c58b68dedffcfb322a5e6a585a188dc37c
Ilder Gutierrez <125604310+ildergutierrez@users.noreply.github.com>
2024-05-12 12:08:08 -0500

    ...

--- php/conexion_bd.php
+++ php/conexion_bd.php
@@ -1,17 +1,26 @@
 <?php
 // Credenciales de la base de datos
-$host = getenv('roundhouse.proxy.rlwy.net');
-$username = "root";
-$password = "foRywFWBeMEuYBfMvJhAWfzYBybpgGVI";
-$database =  getenv('railway');
+session_start();
 
-// Conexión a la base de datos
-$conexion = mysqli_connect($host, $username, $password, $database);
+$BD_HOST = $_ENV["BD_HOST"];
+$BD_USER = $_ENV["BD_USER"];
+$BD_PASSWORD = $_ENV["BD_PASSWORD"];
+$BD_NAME = $_ENV["BD_NAME"];
+$BD_PORT = $_ENV["BD_PORT"];
 
-// Verificar la conexión
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-} else {
-    echo "Conexión exitosa";
-}
+$BD = mysqli_connect("$BD_HOST", "$BD_USER", "$BD_PASSWORD", "$BD_NAME", "$BD_PORT");
+// $host = getenv('roundhouse.proxy.rlwy.net');
+// $username = 'root';
+// $password = 'foRywFWBeMEuYBfMvJhAWfzYBybpgGVI';
+// $database =  getenv('railway');
+// $port = '48465';
 
+// // Conexión a la base de datos
+// $conexion = mysqli_connect("$host", "$username", "$password", "$database", "$port");
+
+// // Verificar la conexión
+// if ($conexion->connect_error) {
+//     die("Error de conexión: " . $conexion->connect_error);
+// } else {
+//     echo "Conexión exitosa";
+// }
